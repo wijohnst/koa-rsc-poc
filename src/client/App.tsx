01fd03826925cd946d6627f24893db28f26582de
Will Johnston <wijohnst@gmail.com>
2024-05-26 12:58:24 -0700

    chore: adds react components

---
+++ src/client/App.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const App = (): React.ReactElement => {
+  return <span>App Works</span>;
+};
+
+export default App;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,30 @@
+import * as React from "react";
 import Router from "@koa/router";
+import fs from "fs";
+import { renderToString } from "react-dom/server";
+
+import App from "../client/App";
 
 const router = new Router();
 
 router.get("/", (ctx) => {
-  ctx.body = "Hello World!!!";
+  const html = `
+  <html>
+    <head></head>
+    <body>
+       <div id="root">${renderToString(React.createElement(App))}</div> 
+       <script src="/client.js"></script>
+    </body>
+  </html>
+  `;
+
+  ctx.body = html;
+  return ctx.body;
+});
+
+router.get("/client.js", (ctx) => {
+  ctx.type = "application/javascript";
+  ctx.body = fs.readFileSync("../client/App.js", "utf-8");
   return ctx.body;
 });
 
